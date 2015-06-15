d961bb280ad4b01df33058d1ebbe16cce5055cf2
Seth Brasile <seth.brasile@gmail.com>
2015-06-15 15:46:52 -0500

    Added messages

--- addon/messages.js
+++ addon/messages.js
@@ -1,5 +1,5 @@
 export default {
-  presenceMessage: 'can\`t be blank',
+  presenceMessage: "can't be blank",
   absenceMessage: 'must be blank',
   inclusionMessage: 'is not included in the list',
   exclusionMessage: 'is reserved',
@@ -11,7 +11,10 @@ export default {
   acceptanceMessage: 'must be accepted',
   zipCodeMessage: 'is not a valid zip code',
   URLMessage: 'must be a valid url',
-  wrongLengthMessage: 'is the wrong length (should be %@ characters)',
-  tooShortMessage: 'is too short (minimum is %@ characters)',
-  tooLongMessage: 'is too long (maximum is %@ characters)'
+  maxLengthMessage: 'is too long. Maximum length is %@ characters',
+  minLengthMessage: 'is too short. Minimum length is %@ characters',
+  mustContainNumberMessage: 'must include a number',
+  mustContainSpecialMessage: 'must include one of these special characters: %@',
+  mustContainLowerMessage: 'must include a lower case character',
+  mustContainCapitalMessage: 'must include an upper case character'
 };
