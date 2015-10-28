9b73079b3dd170904afe5f70164fa7f8cda7f04b
Sebastián Duque <sduquej@gmail.com>
2015-10-28 00:03:09 +0000

    Changes all strings to single quote for consistency

--- addon/messages.js
+++ addon/messages.js
@@ -1,8 +1,9 @@
 export default {
-  presenceMessage: "can't be blank",
+  presenceMessage: 'can\'t be blank',
   absenceMessage: 'must be blank',
   inclusionMessage: 'is not included in the list',
   exclusionMessage: 'is reserved',
+  
   // Numericality
   numericalityMessage: 'is not a number',
   numericalityOnlyIntegerMessage: 'must be an integer',
@@ -21,9 +22,9 @@ export default {
   acceptanceMessage: 'must be accepted',
   zipCodeMessage: 'is not a valid zip code',
   URLMessage: 'must be a valid url',
-  wrongLengthMessage: "is the wrong length (should be %@ characters)",
-  tooShortMessage: "is too short (minimum is %@ characters)",
-  tooLongMessage: "is too long (maximum is %@ characters)",
+  wrongLengthMessage: 'is the wrong length (should be %@ characters)',
+  tooShortMessage: 'is too short (minimum is %@ characters)',
+  tooLongMessage: 'is too long (maximum is %@ characters)',
   mustContainNumberMessage: 'must include a number',
   mustContainSpecialMessage: 'must include one of these special characters: %@',
   mustContainLowerMessage: 'must include a lower case character',
