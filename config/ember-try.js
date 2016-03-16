105fbbc118fbd672c495c7426d32e562d90b7fde
Esteban Arango Medina <marranoparael31@gmail.com>
2016-03-15 22:50:27 -0700

    remove old ember try

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,13 +4,13 @@ module.exports = {
       name: 'default',
       dependencies: { }
     },
-    {
-      name: 'ember-1.10-with-ember-data-old',
-      dependencies: {
-        'ember': '1.10.0',
-        'ember-data': '1.0.0-beta.16'
-      }
-    },
+    // {
+    //   name: 'ember-1.10-with-ember-data-old',
+    //   dependencies: {
+    //     'ember': '1.10.0',
+    //     'ember-data': '1.0.0-beta.16'
+    //   }
+    // },
     {
       name: 'ember-release',
       dependencies: {
