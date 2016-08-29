154257ec613ab5f6f73b13737b0404d7635331f2
Esteban Arango Medina <marranoparael31@gmail.com>
2016-08-28 23:16:01 -0500

    Fruits dropdown for presence.

--- tests/dummy/app/controllers/validators/presence.js
+++ tests/dummy/app/controllers/validators/presence.js
@@ -1,5 +1,6 @@
 import BaseContoller from './base';
 
 export default BaseContoller.extend({
-  modelName: 'for-presence'
+  modelName: 'for-presence',
+  fruits: ['Spanish Lime','Soursop','Mamey Sapote','Alibertia patinoi','Mango']
 });
