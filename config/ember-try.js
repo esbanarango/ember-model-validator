a646311713d2c6996743d57c4351c2ea90f57698
Esteban Arango Medina <esteban.arango@envoy.com>
2021-09-02 16:38:22 -0500

    Include ember data in try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,47 +3,52 @@
 const getChannelURL = require('ember-source-channel-url');
 
 module.exports = function () {
-  return Promise.all([getChannelURL('release'), getChannelURL('beta'), getChannelURL('canary')]).then((urls) => {
+  return Promise.all([getChannelURL('release'), getChannelURL('beta')]).then((urls) => {
     return {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-3.4',
+          name: 'ember-lts-3.4-and-ember-data-3.4',
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0',
+              'ember-source': '~3.4.8',
+              'ember-data': '~3.4.4',
             },
           },
         },
         {
-          name: 'ember-lts-3.8',
+          name: 'ember-lts-3.8-and-ember-data-3.8',
           npm: {
             devDependencies: {
-              'ember-source': '~3.8.0',
+              'ember-source': '~3.8.3',
+              'ember-data': '~3.8.1',
             },
           },
         },
         {
-          name: 'ember-lts-3.16',
+          name: 'ember-lts-3.16-and-ember-data-3.16',
           npm: {
             devDependencies: {
-              'ember-source': '~3.8.0',
+              'ember-source': '~3.16.10',
+              'ember-data': '~3.16.9',
             },
           },
         },
         {
-          name: 'ember-lts-3.20',
+          name: 'ember-lts-3.20-and-ember-data-3.20',
           npm: {
             devDependencies: {
-              'ember-source': '~3.8.0',
+              'ember-source': '~3.20.7',
+              'ember-data': '~3.20.5',
             },
           },
         },
         {
-          name: 'ember-lts-3.24',
+          name: 'ember-lts-3.24-and-ember-data-3.24',
           npm: {
             devDependencies: {
-              'ember-source': '~3.8.0',
+              'ember-source': '~3.24.3',
+              'ember-data': '~3.24.2',
             },
           },
         },
@@ -60,14 +65,6 @@ module.exports = function () {
           npm: {
             devDependencies: {
               'ember-source': urls[1],
-            },
-          },
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2],
             },
           },
         },
