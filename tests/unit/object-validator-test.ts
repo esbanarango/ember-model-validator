467eb906369628ce4ee37ab5cde987bd366e7bbf
Esteban Arango Medina <marranoparael31@gmail.com>
2023-01-03 13:02:27 -0500

    Fix object validator

--- tests/unit/object-validator-test.ts
+++ tests/unit/object-validator-test.ts
@@ -29,4 +29,30 @@ module('Unit | object-validator', function (hooks) {
     assert.false(subject.validate());
     assert.strictEqual(subject.get('errors').errorsFor('name').mapBy('message')[0][0], Messages.presenceMessage);
   });
+
+  test('Re computed errors', function (assert) {
+    assert.expect(4);
+
+    interface ValidatorObjectClass extends ValidatedObject, EmberObject {}
+
+    @objectValidator
+    class ValidatorObjectClass extends EmberObject {
+      name = '';
+      _locale = 'pt-br';
+      validations = {
+        name: {
+          presence: true,
+        },
+      };
+    }
+    const subject = ValidatorObjectClass.create();
+    subject.name = '';
+
+    assert.false(subject.validate());
+    assert.strictEqual(subject.get('errors').errorsFor('name').mapBy('message')[0][0], Messages.presenceMessage);
+
+    subject.name = 'test';
+    assert.true(subject.validate());
+    assert.strictEqual(subject.get('errors').errorsFor('name').length, 0);
+  });
 });
