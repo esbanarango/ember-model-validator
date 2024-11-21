28717af3532668f08b5322f8b774fb8c61bbc36a
Esteban <marranoparael31@gmail.com>
2024-11-21 10:18:16 -0500

    f

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,6 +1,6 @@
 import Application from 'dummy/app';
+import QUnit from 'qunit';
 import config from 'dummy/config/environment';
-import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
