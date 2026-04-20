d545f90994aade2e051a5496d0f3971fcd1de6ca
BobrImperator <bartholomew.dudzik@gmail.com>
2026-04-20 10:54:50 +0200

    chore: simplify ember-data config setup

--- babel.config.cjs
+++ babel.config.cjs
@@ -4,60 +4,18 @@
  * (and linting)
  */
 const { buildMacros } = require('@embroider/macros/babel');
+const { setConfig } = require('@warp-drive/build-config/cjs-set-config.cjs');
 
 const {
   babelCompatSupport,
   templateCompatSupport,
 } = require('@embroider/compat/babel');
 
-const macros = buildMacros();
-
-// ember-data packages use @embroider/macros getGlobalConfig().WarpDrive
-// Inject WarpDrive config into the macros babel plugin config
-const macroPlugin = macros.babelMacros[0];
-if (Array.isArray(macroPlugin)) {
-  macroPlugin[1].globalConfig.WarpDrive = {
-    env: { DEBUG: true, PRODUCTION: false, TESTING: true },
-    debug: {
-      LOG_GRAPH: false,
-      LOG_CACHE: false,
-      LOG_IDENTIFIERS: false,
-      LOG_INSTANCE_CACHE: false,
-      LOG_NOTIFICATIONS: false,
-      LOG_REQUESTS: false,
-      LOG_METRIC_COUNTS: false,
-      DEBUG_RELATIONSHIP_NOTIFICATIONS: false,
-      __INTERNAL_LOG_NATIVE_MAP_SET_COUNTS: false,
-    },
-    activeLogging: {
-      LOG_GRAPH: false,
-      LOG_CACHE: false,
-      LOG_IDENTIFIERS: false,
-      LOG_INSTANCE_CACHE: false,
-      LOG_NOTIFICATIONS: false,
-      LOG_REQUESTS: false,
-      LOG_METRIC_COUNTS: false,
-      DEBUG_RELATIONSHIP_NOTIFICATIONS: false,
-      __INTERNAL_LOG_NATIVE_MAP_SET_COUNTS: false,
-    },
-    deprecations: {
-      DEPRECATE_COMPUTED_CHAINS: true,
-      DEPRECATE_NON_STRICT_TYPES: true,
-      DEPRECATE_NON_STRICT_ID: true,
-      DEPRECATE_NON_UNIQUE_PAYLOADS: true,
-      DEPRECATE_RELATIONSHIP_REMOTE_UPDATE_CLEARING_LOCAL_STATE: true,
-      DEPRECATE_MANY_ARRAY_DUPLICATES: true,
-      DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: true,
-      DEPRECATE_EMBER_INFLECTOR: true,
-      ENABLE_LEGACY_SCHEMA_SERVICE: true,
-    },
-    features: {
-      JSON_API_CACHE_VALIDATION_ERRORS: false,
-    },
-    includeDataAdapter: true,
-    polyfillUUID: false,
-  };
-}
+const macros = buildMacros({
+  configure: (config) => {
+    setConfig(config, {});
+  },
+});
 
 // For scenario testing
 const isCompat = Boolean(process.env.ENABLE_COMPAT_BUILD);
